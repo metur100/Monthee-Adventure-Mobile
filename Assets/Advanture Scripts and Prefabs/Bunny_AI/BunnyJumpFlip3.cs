6be6390ca9c717fcf349322ea9ff35deca0a361a
mturkes <medin_93@live.com>
2021-08-31 12:17:43 +0200

    Fix bunny jump and flip
    Sometimes it got stuck

---
+++ Assets/Advanture Scripts and Prefabs/Bunny_AI/BunnyJumpFlip3.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunnyJumpFlip3 : MonoBehaviour
+{
+    private bool dirRight = true;
+    public float speed;
+    public Animator animator;
+    public Rigidbody2D rb;
+    void Update()
+    {
+        if (dirRight)
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+            transform.localScale = new Vector2(-50, 50);
+        }
+
+        else
+        {
+            transform.localScale = new Vector2(50, 50);
+            transform.Translate(-Vector2.right * speed * Time.deltaTime);
+        }
+
+        if (transform.position.x >= 7011f)
+        {
+            dirRight = false;
+        }
+
+        if (transform.position.x <= 6730f)
+        {
+            dirRight = true;
+        }
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        switch (col.tag)
+        {
+            case "BigStump":
+                rb.AddForce(Vector2.up * 2000f);
+                animator.SetTrigger("IsJumping");
+                break;
+        }
+    }
+}
